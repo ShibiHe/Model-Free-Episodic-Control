fd232afda2e7074ba450aad9f57df8c118db0a67
Frank He <frankheshibi@gmail.com>
2016-08-17 03:20:41 -0500

    fix testing

--- dqn_ep/ale_agents.py
+++ dqn_ep/ale_agents.py
@@ -703,29 +703,8 @@ class EC_DQN(object):
 
         # TESTING---------------------------
         if self.testing:
-            self.test_data_set.add_sample(self.last_img, self.last_action, np.clip(reward, -1, 1), False)
-            if self.step_counter >= self.phi_length:
-                phi = self.test_data_set.phi(observation)
-                q_values = self.network.q_vals(phi)
-                action1 = np.argmax(q_values)
-                return1 = q_values[action1]
-            else:
-                action1 = 0
-                return1 = -1
-
-            action2 = 0
-            return2 = 0
-            for a in range(self.num_actions):
-                return_a = self.qec_table.estimate(observation, a)
-                if return_a > return2:
-                    return2 = return_a
-                    action2 = a
-            print action1, return1, action2, return2
-            raw_input()
-            if return2 > return1*1.5:
-                action = action2
-            else:
-                action = action1
+            action = self._choose_action(self.test_data_set, 0.0,
+                                         observation, np.clip(reward, -1, 1))
 
         # NOT TESTING---------------------------
         else:
